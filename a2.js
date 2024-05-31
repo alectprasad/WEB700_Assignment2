4795d70603b4e8c6fdcc408d3d7eecf0a148e888
alectprasad <alectprasad@gmail.com>
2024-05-31 17:34:11 -0400

    add declaration

--- a2.js
+++ a2.js
@@ -1,3 +1,12 @@
+/*********************************************************************************
+* WEB700 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Alect Prasad Student ID: 101977239 Date: 5/31/2024
+*
+********************************************************************************/
 const collegeData = require('./modules/collegeData');
 //call function to read data
 collegeData.initialize()
